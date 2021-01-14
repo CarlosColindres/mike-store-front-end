6461a488b749eb0ba2e7a9a04d857c54baeb6ffa
CarlosColindres <70364536+CarlosColindres@users.noreply.github.com>
2021-01-14 10:55:59 -0500

    Delete client directory

--- client/src/redux/reducers/root-reducer.ts
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-import cartStateReducer from './cartStateReducer'
-import modalStateReducer from './modalStateReducer'
-import categoryStateReducer from './categoryStateReducer'
-export default combineReducers({
-    modalState: modalStateReducer,
-    cart: cartStateReducer,
-    categoryState: categoryStateReducer
-})
